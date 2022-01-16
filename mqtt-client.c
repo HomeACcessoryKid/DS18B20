3cb80750ea568ff566206c13e40bcae7ad931b15
HomeACcessoryKid <homeaccessorykid@gmail.com>
2022-01-16 16:51:19 +0100

    0.1.3 replaced wifi code and no backoff reset after network connect
    and cosmetics

--- mqtt-client.c
+++ mqtt-client.c
@@ -2,7 +2,6 @@
 #include "mqtt-client.h"
 
 QueueHandle_t publish_queue;
-SemaphoreHandle_t wifi_alive;
 char    *mqtthost=NULL, *mqttuser=NULL, *mqttpass=NULL, *dmtczidx=NULL;
 
 static const char *  get_my_id(void) {
@@ -30,10 +29,10 @@ static const char *  get_my_id(void) {
 
 #define BACKOFF1 100/portTICK_PERIOD_MS
 static void  mqtt_task(void *pvParameters) {
-    int ret         = 0;
+    int ret = 0;
     int backoff = BACKOFF1;
     struct mqtt_network network;
-    mqtt_client_t client   = mqtt_client_default;
+    mqtt_client_t client = mqtt_client_default;
     char mqtt_client_id[20];
     uint8_t mqtt_buf[100];
     uint8_t mqtt_readbuf[100];
@@ -42,22 +41,21 @@ static void  mqtt_task(void *pvParameters) {
 
     mqtt_network_new( &network );
     memset(mqtt_client_id, 0, sizeof(mqtt_client_id));
-    strcpy(mqtt_client_id, "ESP-");
+    strcpy(mqtt_client_id, "MAC-");
     strcat(mqtt_client_id, get_my_id());
 
-    data.willFlag       = 0;
-    data.MQTTVersion    = 3;
+    data.willFlag           = 0;
+    data.MQTTVersion        = 3;
     data.clientID.cstring   = mqtt_client_id;
     data.username.cstring   = mqttuser;
     data.password.cstring   = mqttpass;
     data.keepAliveInterval  = 10;
-    data.cleansession   = 0;
+    data.cleansession       = 0;
 
+    printf("%s: started\n", __func__);
     while(1) {
-        //xSemaphoreTake(wifi_alive, portMAX_DELAY);
-        printf("%s: started\n", __func__);
-        printf("%s: (Re)connecting to MQTT server %s ... ",__func__,
-               mqtthost);
+        while (sdk_wifi_station_get_connect_status() != STATION_GOT_IP) vTaskDelay(200/portTICK_PERIOD_MS); //Check if we have an IP
+        printf("%s: (re)connecting to MQTT server %s ... ",__func__, mqtthost);
         ret = mqtt_network_connect(&network, mqtthost, MQTT_PORT);
         if( ret ){
             printf("error: %d\n", ret);
@@ -66,11 +64,8 @@ static void  mqtt_task(void *pvParameters) {
             continue;
         }
         printf("done\n");
-        backoff = BACKOFF1;
-        mqtt_client_new(&client, &network, 5000, mqtt_buf, 100,
-                      mqtt_readbuf, 100);
-
-        printf("Send MQTT connect ... ");
+        mqtt_client_new(&client, &network, 5000, mqtt_buf, 100, mqtt_readbuf, 100);
+        printf("%s: send MQTT connect ... ", __func__);
         ret = mqtt_connect(&client, &data);
         if(ret){
             printf("error: %d\n", ret);
@@ -83,11 +78,9 @@ static void  mqtt_task(void *pvParameters) {
         backoff = BACKOFF1;
         xQueueReset(publish_queue);
 
-        while(1){
-
+        while(1) {
             msg[PUB_MSG_LEN - 1] = 0;
             while(xQueueReceive(publish_queue, (void *)msg, 0) == pdTRUE){
-//                 printf("got message to publish\n");
                 mqtt_message_t message;
                 message.payload = msg;
                 message.payloadlen = strlen(msg);
@@ -96,54 +89,15 @@ static void  mqtt_task(void *pvParameters) {
                 message.retained = 0;
                 ret = mqtt_publish(&client, MQTT_topic , &message);
                 if (ret != MQTT_SUCCESS ){
-                    printf("error while publishing message: %d\n", ret );
+                    printf("%s: error while publishing message: %d\n", __func__, ret );
                     break;
                 }
             }
-
             ret = mqtt_yield(&client, 1000);
-            if (ret == MQTT_DISCONNECTED)
-                break;
+            if (ret == MQTT_DISCONNECTED) break;
         }
-        printf("Connection dropped, request restart\n");
+        printf("%s: connection dropped, connecting again\n", __func__);
         mqtt_network_disconnect(&network);
-        taskYIELD();
-    }
-}
-
-static void  wifi_task(void *pvParameters) {
-    uint8_t status  = 0;
-    uint8_t retries = 30;
-    while(1) {
-        while ((status != STATION_GOT_IP) && (retries)){
-            status = sdk_wifi_station_get_connect_status();
-            printf("%s: status = %d\n", __func__, status );
-            if( status == STATION_WRONG_PASSWORD ){
-                printf("WiFi: wrong password\n");
-                break;
-            } else if( status == STATION_NO_AP_FOUND ) {
-                printf("WiFi: AP not found\n");
-                break;
-            } else if( status == STATION_CONNECT_FAIL ) {
-                printf("WiFi: connection failed\n");
-                break;
-            }
-            vTaskDelay( 1000 / portTICK_PERIOD_MS );
-            --retries;
-        }
-        if (status == STATION_GOT_IP) {
-            printf("WiFi: Connected\n");
-            xSemaphoreGive( wifi_alive );
-            taskYIELD();
-        }
-
-        while ((status = sdk_wifi_station_get_connect_status()) == STATION_GOT_IP) {
-            xSemaphoreGive( wifi_alive );
-            taskYIELD();
-        }
-        printf("WiFi: disconnected\n");
-        sdk_wifi_station_disconnect();
-        vTaskDelay( 1000 / portTICK_PERIOD_MS );
     }
 }
 
@@ -163,9 +117,7 @@ static void ota_string() {
 }
 
 void mqtt_client_init(int queue_size) {
-    vSemaphoreCreateBinary(wifi_alive);
     publish_queue = xQueueCreate(queue_size, PUB_MSG_LEN);
     ota_string();
-    xTaskCreate(&wifi_task, "wifi_task",  256, NULL, 1, NULL);
     xTaskCreate(&mqtt_task, "mqtt_task", 1024, NULL, 2, NULL);
 }
